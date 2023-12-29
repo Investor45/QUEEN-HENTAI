7b54eaf25607aae6fd8a24f4fe5431f836521b81
Investor45 <133650270+Investor45@users.noreply.github.com>
2023-12-29 15:51:40 -0800

    Update config.js

--- config.js
+++ config.js
@@ -47,19 +47,19 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = '💝 Queen Hentai 💝'
+global.botname = '💝MR.KEN💝'
 global.premium = 'true'
-global.packname = '💝 Queen Hentai 💝 Created By 💝 Dinuwa Official²⁰²³ 💝' 
-global.author = '💝 Dinuwa Official²⁰²³ 💝' 
-global.igfg = '▢ Follow on Facebook\https://www.facebook.com/profile.php?id=61551915615888\n' 
-global.dygp = 'https://chat.whatsapp.com/Jyjqx7KBfgjDC7QcefHB1S'
-global.fgsc = 'https://github.com/dinuwah/QUEEN-HENTAI' 
-global.fgyt = 'https://chat.whatsapp.com/Jyjqx7KBfgjDC7QcefHB1S'
-global.fgpyp = 'https://chat.whatsapp.com/Jyjqx7KBfgjDC7QcefHB1S'
+global.packname = '💝 kentech-robot 💝 Created By 💝 Mr.Ken ²⁰²³ 💝' 
+global.author = '💝 Mr.Ken Official²⁰²³ 💝' 
+global.igfg = '▢ Follow on Facebook\https://www.facebook.com/profile.php?id=\n' 
+global.dygp = 'https://chat.whatsapp.com/LcMZEQJA1ps7w0oXP3NzHt'
+global.fgsc = 'https://github.com/Investor45/QUEEN-HENTAI' 
+global.fgyt = 'https://chat.whatsapp.com/LcMZEQJA1ps7w0oXP3NzHt'
+global.fgpyp = 'https://chat.whatsapp.com/LcMZEQJA1ps7w0oXP3NzHt'
 global.fglog = 'https://i.ibb.co/T1DqdYz/thumbnail.jpg' 
 
 
-global.wait = '*💝 _Waiting Bro Uploading by 💝 Queen Hentai 💝..._*'
+global.wait = '*💝 _Waiting Bro Uploading by 💝 Mr.ken💝..._*'
 global.rwait = '🇱🇰'
 global.dmoji = '🤣'
 global.done = '👍'
